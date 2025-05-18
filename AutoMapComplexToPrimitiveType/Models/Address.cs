c426d3a03e3c0286fea2ea3afef145762e781749
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-18 13:33:20 +0100

    Add project files.

---
+++ AutoMapComplexToPrimitiveType/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoMapComplexToPrimitiveType.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+
+        [Required] 
+        public string Street { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public int UserId { get; set; } //Foreign key.
+
+        public User User { get; set; }
+
+    }
+}
---
+++ AutoMapComplexToPrimitiveType/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoMapComplexToPrimitiveType.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        // Complex type: Address object contains detailed address information.
+        public Address Address { get; set; }
+    }
+}
