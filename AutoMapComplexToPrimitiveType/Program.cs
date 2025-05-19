f59ac39e612c13bb0f03d20a87ee41d0db0cc50a
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-19 10:47:18 +0100

    refact(program): Implement connectionstrings. update program

--- AutoMapComplexToPrimitiveType/Program.cs
+++ AutoMapComplexToPrimitiveType/Program.cs
@@ -1,8 +1,20 @@
+using AutoMapComplexToPrimitiveType.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+
+//Register the DbContext with dependency injection
+builder.Services.AddDbContext<UserDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AutoMapperComplexTypeEFCoreDBConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
