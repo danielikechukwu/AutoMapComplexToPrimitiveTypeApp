3f26f911423d940ee13b4ebde74fc9c1f55ec69b
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-19 10:45:25 +0100

    feat(DTOs): Implement entities data transfer objects

---
+++ AutoMapComplexToPrimitiveType/DTOs/UserCreateDTO.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoMapComplexToPrimitiveType.DTOs
+{
+    public class UserCreateDTO
+    {
+        // DTO for creating a new User. The address details are provided as primitive types.
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required] 
+        public string LastName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        // These are primitive properties that will be mapped into a complex Address object.
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+    }
+}
---
+++ AutoMapComplexToPrimitiveType/DTOs/UserDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace AutoMapComplexToPrimitiveType.DTOs
+{
+    public class UserDTO
+    {
+        // DTO for sending User details along with individual address properties (primitive types).
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        // Address properties are primitive types (string) even though they come from a complex Address object.
+        public int AddressId { get; set; }
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+    }
+}
